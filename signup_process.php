56e44b48720e3a622a0fc6d77f7bcdb2391a1f5a
FennecFoxSW <FennecFoxSW@protonmail.com>
2022-10-02 02:54:02 +0900

    Merge orphan branch 'master' into 'main'

---
+++ signup_process.php
@@ -0,0 +1,16 @@
+<?php
+echo $_POST['id'];
+echo $_POST['password'];
+$id =$_POST['id'];
+$password = $_POST['password'];
+$conn=mysqli_connect('127.0.0.1','root','111111','chat');
+$sql=$sql = "
+  INSERT INTO users (name,password)
+    VALUES('{$_POST['id']}','{$_POST['password']}')
+";
+
+mysqli_query($conn,$sql);
+?>
+        <meta http-equiv="refresh" content="0;URL='login.php?ch'">
+<?php
+ ?>
