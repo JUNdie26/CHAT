9357e00997137430dbf545968d2886c142aaf297
JUNdie26 <jhj00664@naver.com>
2022-11-08 11:30:55 +0900

    cas

--- login_process.php
+++ login_process.php
@@ -1,7 +1,5 @@
 
 <?php
-echo $_POST['id'];
-echo $_POST['password'];
 $id =$_POST['id'];
 $password = $_POST['password'];
 $conn=mysqli_connect('localhost','root','wkdguswhd0626!!','chat');
@@ -14,7 +12,7 @@ while($row = mysqli_fetch_array($result)){
       
       <form id='my_frm' action='index.php' method='POST'>    
         <input type='hidden' name='id' value="<?php $_POST['id']?>"> //아이디 정보 index.php로 이동 id=my_frm은 밑에 자스 코드에 필요한거라 나도 잘 모름
-       
+        
       </form>
       <script> // 아이디 비번 일치하면 index.php 페이지로 이동 자스코드
           window.onload=function(){
